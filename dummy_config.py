be0434c09d5cd3388bf65cdc02cf956e96effa78
jasonwoz <jasonwoz@umich.edu>
2019-01-19 01:44:39 -0500

    Added a dummmy config file for login information

---
+++ dummy_config.py
@@ -0,0 +1,7 @@
+# TODO: Replace with your information
+gmail_inbox_address = "https://mail.google.com/mail/u/0/?pli=1#inbox/"
+email = ""
+canvas_username = ""
+canvas_password = ""
+email_query = "日本に行きたい" #"\'from:mailer@doodle.com\'" or whatever you would type into the gmail search bar
+doodle_name = "ジェイソン"
